72caa6d270ce04301ea7cf78b27fa6aa57ddfb53
JyothiS2005 <JyothiHudali05@gmail.com>
2025-06-09 04:42:30 +0530

    origin

---
+++ Student.java
@@ -0,0 +1,30 @@
+class Student {
+    String name;
+    int Id;
+    float Marks;
+    String Departement;
+
+}
+
+    Student(String name, int Id, float Marks, String Department) {
+        this.name = name;
+        this.Id = Id;
+        this.Marks = Marks;
+        this.Departement = Department;
+
+    }
+
+    // to determine the Maximum and minimum score//
+    {
+        System.out.println("john");
+        System.out.println("Kavya");
+    }
+
+    public class Array {
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student;
+        Student.display();
+        
+    }
